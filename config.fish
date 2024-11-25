6c932c3b0018783bae554a5f4427ac5786eaf020
BUZZKILL1549 <aushvs10@hotmail.com>
2024-11-25 13:45:13 +0530

    Config for DNF

--- config.fish
+++ config.fish
@@ -2,60 +2,40 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# General aliases
+# general aliases
 alias ll='ls -la'
-alias fishconf='nvim ~/.config/fish/config.fish'
-alias fishsrc='source ~/.config/fish/config.fish'
+alias fish-conf='nvim ~/.config/fish/config.fish'
+alias fish-src='source ~/.config/fish/config.fish'
 
-# Coding aliases
-function runp -d "Run python script"
-    python $argv
-end
+# git aliases
+alias add='git add .'
+alias commit='git commit -am '
+alias push='git push'
 
-function runc -d "Run C script"
-    make $argv
-    ./$argv
-end
+# programming aliases
+alias runp='python'
+alias runc='cargo run'
 
-# Functions
+# functions
 function update -d "Updates system"
-    sudo pacman -Syyu -y
-    eos-update
+	sudo dnf update -y
 end
 
-function download -a flag package -d "Installs package"
-    if test "$flag" = -P || test "$flag" = -p
-        sudo pacman -S $package
-    else if test "$flag" = -Y || test "$flag" = -y
-        yay -S $package
-    else
-        sudo pacman -S $package
-        if test $status = 1
-            yay -S $package
-            if test $status = 1
-                return "Package not found"
-            end
-        end
-    end
+function upgrade -d "Upgrades system"
+	sudo dnf upgrade -y
 end
 
-function delete -d "Uninstalls package"
-    sudo pacman -R $argv
+function download -a package -d "Installs package"
+	sudo dnf install $package
+	if test $status = 1
+		return "Package not found."
+	end
 end
 
-function search -d "Searches for packages in AUR"
-    read -P "PACMAN[P] / YAY[Y]: " repo
-    if test "$repo" = P || test "$repo" = p
-        pacman -Ss $argv
-    else if test "$repo" = Y || test "$repo" = y
-        yay -Ss $argv
-    end
+function delete -d "Deletes package"
+	sudo dnf remove $argv
 end
 
-function isearch -d "Searches for packages in system"
-    read -P "Package: " package
-    pacman -Qs $package
+function search -d "Searches for package"
+	dnf search $argv
 end
-
-# Created by `pipx` on 2024-08-27 15:37:35
-set PATH $PATH /home/buzzkill/.local/bin
