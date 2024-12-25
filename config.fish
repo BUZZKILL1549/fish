ad3896ba0581363e06b0b228be5a7d519e129e69
BUZZKILL1549 <aushvs10@hotmail.com>
2024-12-25 11:46:25 +0530

    More aliases and some tricky stuff

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@ end
 
 # sources
 source ~/.asdf/asdf.fish
+set -gx PATH $PATH ~/Documents/Projects/flutter/dev/flutter/bin
 
 # general aliases
 alias ll='ls -la'
@@ -20,6 +21,7 @@ alias glog='git log'
 
 # programming aliases
 alias runp='python'
+alias cr-env='python -m venv .venv'
 alias src-env='source .venv/bin/activate.fish'
 alias runcr='cargo run'
 alias runr='ruby'
@@ -28,20 +30,26 @@ function runc -a ofile ifile -d "Compiles C"
     gcc -o $ofile $ifile
 end
 
-function cd -d "Changes directory with automatic env activation" --wraps=cd
-    builtin cd $argv
-
-    if not set -q VIRTUAL_ENV
-        if test -d ./.venv/
-            source ./.venv/bin/activate.fish
-        end
-    else
-        set parentdir (dirname $VIRTUAL_ENV)
-        if not string match -q "$parentdir" "$PWD"/*
-            deactivate
-        end
-    end
+function aconda -d "Activates conda environment"
+    eval "$(/home/buzzkill/anaconda3/bin/conda shell.fish hook)"
+    conda activate $argv
 end
+
+
+#function cd -d "Changes directory with automatic env activation" --wraps=cd
+#builtin cd $argv
+
+#if not set -q VIRTUAL_ENV
+#if test -d ./.venv/
+#source ./.venv/bin/activate.fish
+#end
+#else
+#set parentdir (dirname $VIRTUAL_ENV)
+#if not string match -q "$parentdir" "$PWD"/*
+#deactivate
+#end
+#end
+#end
 
 # functions
 function update -d "Updates system"
@@ -66,3 +74,7 @@ end
 function search -d "Searches for package"
     dnf search $argv
 end
+
+# Created by `pipx` on 2024-12-12 13:24:18
+set PATH $PATH /home/buzzkill/.local/bin
+set -x PATH /usr/local/cuda-12.6/bin $PATH
